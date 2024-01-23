4ae066a463748145f56e4f1ae163697aae91c280
Gianfranco Di Pietro <gianfrancodipietro@gmail,com>
2024-01-23 11:25:00 +0100

    fixed uppercase string variables

--- shp2html.py
+++ shp2html.py
@@ -30,26 +30,19 @@ Javascript libraries used:
 """
 import json
 import os
+
 from zipfile import ZipFile
 from osgeo import ogr
 
+
 # !!! User inputs !!!
 # Change this paths with the path to your shapefile and raster
-shapefile_path = r'sample_data/TOR1/TOR1.shp'
-raster_path = r'sample_data/TOR1/TOR1.jpg'
-project_name = 'TEST' # name of the project, used to name the output files
+SHAPEFILE = r'sample_data/TOR1/TOR1.shp'
+RASTER = r'sample_data/TOR1/TOR1.jpg'
+NAME = 'TEST' # name of the project, used to name the output files
 # !!! End of user inputs !!!
 
 #####################################################################
-# Use True to export as single page HTML, script add header and footer tags
-# to be able to open the file in a browser
-# export_as_html_single_page = True # REMOVED, now always exported
-
-
-# Use True to export the attribute table as json file separated from the html
-# If false the attribute table will be not exported.
-# export_attribute_table_to_json = True # REMOVED, now always exported
-
 #####################################################################
 
 def fix_coords(coords, origin='top-left'):
@@ -86,7 +79,7 @@ def fix_coords(coords, origin='top-left'):
 
 
 # Set a global variable that tracks the occurrence of inner rings in polygons
-inner_rings_detected = 0
+INRINGS = 0
 
 # Set a global variable that holds the list of vertices of each polygon
 polygons_vert = []
@@ -94,7 +87,7 @@ polygons_vert = []
 attributes_list = []
 
 # Open the shapefile
-shapefile = ogr.Open(shapefile_path)
+shapefile = ogr.Open(SHAPEFILE)
 
 # Get the layer
 layer = shapefile.GetLayer()
@@ -108,7 +101,7 @@ for feature in layer:
     # Track inner rings
     n_rings = geometry.GetGeometryCount()
     if n_rings > 1:
-        inner_rings_detected += n_rings-1
+        INRINGS += n_rings-1
 
     # We only get vertices of outer ring (= first element of GetGeometryRef)
     vertices = geometry.GetGeometryRef(0).GetPoints()
@@ -116,62 +109,49 @@ for feature in layer:
 
 
 # Send warning if inner rings (holes) have been detected
-if inner_rings_detected:
-    print(f'Warning: {inner_rings_detected} holes detected. Not handled.')
+if INRINGS:
+    print(f'Warning: {INRINGS} holes detected. Not handled.')
 
+    # HTML header and footer part
 
-    # Generate HTML code wiht header and footer tags
-# if export_as_html_single_page == True: # REMOVED, now always exported
-html_head = '<!DOCTYPE html>\n<html>\n<head>\n\t<title>Image Map</title>\n'\
+HTMLHEAD = '<!DOCTYPE html>\n<html>\n<head>\n\t<title>Image Map</title>\n'\
     '\t<meta charset="utf-8">\n\t<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>\n'\
     '\t<script src="https://cdnjs.cloudflare.com/ajax/libs/image-map-resizer/1.0.10/js/imageMapResizer.min.js"></script>\n'\
         '</head>\n<body>\n'   
-html_foot = '<script>\n\t$(document).ready(function() {\n\t $(\'map\').imageMapResize();\n\t}); \n</script>\n</body>\n</html>'    
+HTMLFOOT = '<script>\n\t$(document).ready(function() {\n\t $(\'map\').imageMapResize();\n\t}); \n</script>\n</body>\n</html>'    
 
-# Generate HTML code wihtout header and footer tags # REMOVED, now always exported
-# else:
-#     html_head = ''
-#     html_foot = ''
-#    print('Warning: HTML file will not be a single page. '\
-   
     
-image_block = f'<img src="{raster_path}" usemap="#image-map" '\
+image_block = f'<img src="{RASTER}" usemap="#image-map" '\
                'style="width: 100%; height: auto;">\n'
 
-polygons_block = '<map name="image-map">\n'
+HTMLPOLYGONS = '<map name="image-map">\n'
 for n, p in enumerate(polygons_vert, start=1):
-    coords_str = str(p)[1:-1].replace('(','').replace(')','').replace(' ','')
+    COORDSSTR = str(p)[1:-1].replace('(','').replace(')','').replace(' ','')
     line = f'\t<area target="_blank" id="imgzone{n}" '\
            f'alt="Element #{n}" title="Element #{n}" '\
-           f'coords="{coords_str}" shape="poly">\n'
-    polygons_block += line
-polygons_block += '</map>\n'
+           f'coords="{COORDSSTR}" shape="poly">\n'
+    HTMLPOLYGONS += line
+HTMLPOLYGONS += '</map>\n'
 
+html_path = rf'{NAME}.html'
+AttributeTable_path = rf'{NAME}.json'
 
 # Save as file
-with open(f'{project_name}.html', 'w') as out_file:
-    out_file.write(html_head)
+with open(html_path, 'w', encoding="utf-8") as out_file:
+    out_file.write(HTMLHEAD)
     out_file.write(image_block)
     out_file.write('\n')
-    out_file.write(polygons_block)
-    out_file.write(html_foot)
+    out_file.write(HTMLPOLYGONS)
+    out_file.write(HTMLFOOT)
 
-
-# if export_attribute_table_to_json == True:
-with open(f'{project_name}.json', 'w') as f:
+with open(f'{NAME}.json', 'w', encoding="utf-8") as f:
     json.dump(attributes_list, f)
 
-# Required inputs
-# raster_path = r'sample_data/TOR1/TOR1.jpg'
-html_path = rf'{project_name}.html'
-# js_path = r'imageMapResizer.min.js'
-AttributeTable_path = rf'{project_name}.json'
 
 # Create zip file
-with ZipFile(rf'{project_name}.zip', 'w') as outzip:
-    outzip.write(raster_path)
+with ZipFile(rf'{NAME}.zip', 'w') as outzip:
+    outzip.write(RASTER)
     outzip.write(html_path)
-    # outzip.write(js_path)
     outzip.write(AttributeTable_path)
 
 # Remove files
