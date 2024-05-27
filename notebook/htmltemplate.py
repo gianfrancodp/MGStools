b2161d8ef28f1d965ec643b9db146552594eb3f0
Gianfranco Di Pietro <gianfrancodipietro@gmail,com>
2024-05-27 11:56:12 +0200

    frist attemp to move part of html template in a separate file.

---
+++ notebook/htmltemplate.py
@@ -0,0 +1,7 @@
+
+
+HTMLHEAD = '<!DOCTYPE html>\n<html>\n<head>\n\t<title>Image Map</title>\n'\
+    '\t<meta charset="utf-8">\n\t<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>\n'\
+    '\t<script src="https://cdnjs.cloudflare.com/ajax/libs/image-map-resizer/1.0.10/js/imageMapResizer.min.js"></script>\n'\
+        '</head>\n<body>\n'   
+HTMLFOOT = '<script>\n\t$(document).ready(function() {\n\t $(\'map\').imageMapResize();\n\t}); \n</script>\n</body>\n</html>'    
