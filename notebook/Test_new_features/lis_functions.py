69e177f00c4ecb5e8cb2ee83ad241dd92007a250
Gianfranco Di Pietro <gianfrancodipietro@gmail,com>
2024-09-10 16:35:55 +0200

    first test to create openlayer viewer PASSed. Move 2 reference file in sample_data from PAL folder to upper folder

--- notebook/Test_new_features/lis_functions.py
+++ notebook/Test_new_features/lis_functions.py
@@ -4,6 +4,7 @@ def upload_files_widgets():
     in Jupyter Notebook
 
     NOTE: syncronize with save_to_temp_dir() the name of file and the directory
+    TODO: change Jpg name reference to generic raster name reference
     '''
     import ipywidgets as widgets
     from IPython.display import display
@@ -33,21 +34,53 @@ def upload_files_widgets():
     labelEPSG = widgets.Label(value='Select the EPSG code of files (not work with custom projection)')
     disclaimer = widgets.Label(value='Note: ouptut folder will be created in the same directory as the input files')
     display(labelShp, Shapefile_selector, labelImg, Jpgfile_selector, labelName, projectname, labelEPSG, EPSGcode, disclaimer)
+    return Shapefile_selector, Jpgfile_selector, projectname, EPSGcode
 
-def save_to_temp_dir():
+
+
+def save_to_temp_dir(Shapefile_selector, Jpgfile_selector):
     '''
     Save to temporary directory the file uploaded by the user
     in Jupyter Notebook using upload_files_widgets()
 
     NOTE: syncronize with upload_files_widgets() the name of file and the directory
+    TODO: change Jpg name reference to generic raster name reference
     '''
-    #create a temporary folder to store the uploaded files
     import os
+    
     # Create a temporary directory
     temp_dir = 'temporary_files'
     if not os.path.exists(temp_dir):
         os.makedirs(temp_dir)
+
     # Save the uploaded files to the temporary directory
+
+    # RASTER file
+
+    jpgfile_data = next(iter(Jpgfile_selector.value))
+    jpgfile_name = jpgfile_data['name']
+    # set raster path to the variable
+    Raster_path = os.path.join(temp_dir, jpgfile_name)
+    with open(Raster_path, 'wb') as f:
+        f.write(jpgfile_data['content'])
+    
+
+    # Shapefile files save inside the temporary directory
+    
+    shapefiledata = iter(Shapefile_selector.value)
+    for filename in shapefiledata:
+        with open(os.path.join(temp_dir, filename['name']), 'wb') as f:
+            f.write(filename['content'])
+    # set shp path to the variable
+    for filename in iter(Shapefile_selector.value):
+        if filename['name'].endswith('.shp'):
+            Shp_file_path = os.path.join(temp_dir, filename['name'])
+    
+
+    return Raster_path, Shp_file_path
+   
+
+
 
     # # Get the uploaded files from the widgets
     # shapefiledata = iter(Shapefile_selector.value)
