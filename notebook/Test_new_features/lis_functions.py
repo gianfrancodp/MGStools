fff915066efdb3266bc087992b7de925be8a398f
Gianfranco Di Pietro <gianfrancodipietro@gmail,com>
2024-09-23 12:38:00 +0200

    1. added cache python to .gitignore     2. fixed and added functions to lis_functions.py     2.1 fix save_to_temp_dir function, to save in the project name gived by user     2.2 change return to samve_to_temp_dir function that create a dictionary to file saved
        2.3 added run_gdal2tiles function as tested before
    
        3. created geojson_template.js file, the function `create_geojson_js` and `append_ja_to_html` for the feature to update the default webviewer generated by gdal2tiles. Now it overlay the vector feature provided by user
    
        4. added test file in a folder called test_PAL11

--- notebook/Test_new_features/lis_functions.py
+++ notebook/Test_new_features/lis_functions.py
@@ -1,3 +1,8 @@
+import os
+import sys
+import subprocess
+from bs4 import BeautifulSoup
+    
 def upload_files_widgets():
     '''
     Using ipywidgets to create a file upload widget for the file upload
@@ -43,7 +48,7 @@ def upload_files_widgets():
     return Shapefile_selector, Jpgfile_selector, projectname
 
 
-def save_to_temp_dir(Shapefile_selector, Jpgfile_selector):
+def save_to_temp_dir(Shapefile_selector, Jpgfile_selector, projectname):
     '''
     Save to temporary directory the file uploaded by the user
     in Jupyter Notebook using upload_files_widgets()
@@ -51,12 +56,17 @@ def save_to_temp_dir(Shapefile_selector, Jpgfile_selector):
     NOTE: syncronize with upload_files_widgets() the name of file and the directory
     TODO: change Jpg name reference to generic raster name reference
     '''
-    import os
+    
     
     # Create a temporary directory
-    temp_dir = 'temporary_files'
+    temp_dir = projectname
+
     if not os.path.exists(temp_dir):
         os.makedirs(temp_dir)
+    else:
+        print("WARNING! - project name directory already exists. please rename it, or delete it before.")
+        print("No file saved. Operation aborted.")
+        return
 
     # Save the uploaded files to the temporary directory
 
@@ -81,18 +91,98 @@ def save_to_temp_dir(Shapefile_selector, Jpgfile_selector):
         if filename['name'].endswith('.shp'):
             Shp_file_path = os.path.join(temp_dir, filename['name'])
     
-
-    return Raster_path, Shp_file_path
+    # a list of paths to the files
+    files_path = {}
+    files_path['Raster_path'] = Raster_path
+    files_path['Shp_file_path'] = Shp_file_path
+    return files_path
    
 
+def run_gdal2tiles(input_raster, output_directory):
+    '''
+    Run gdal2tiles.py command to create tiles from a raster file
+    '''
+    # Check if the output directory exists
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+    else:
+        print("WARNING! - Directory already exists, Files will be overwrited! Continue? (Y).")
+        user_input = input().strip().upper()
+        if user_input != 'Y':
+            print("Operation aborted by the user.")
+            return
+       
+    # command gdal2tiles.py parameters setting
+    gdal2tiles_command = [
+        'gdal2tiles.py',    # default gdal2tiles command
+        '-p', 'raster',     # profile tiles raster
+        '-z', '0-5',        # zoom levels
+        input_raster,       # input raster file
+        output_directory    # output directory
+    ]
+    try:
+        subprocess.run(gdal2tiles_command, check=True)
+        print(f'Tiles stored in: {output_directory}')
+    except subprocess.CalledProcessError as e:
+        print(f'{e}')
+    return
+
+def create_geojson_js(geojson_file_path, template_js_path, destination_path):
+    """
+    Create a `destination_path` file with the GeoJSON file path.
+    destination_path must be a .js file.
+    destination path folder must be the same of the project name.
+
+    Args:
+        geojson_file_pat (str): path to the GeoJSON file.
+        template_js_path (str): path to the OpenLayers GeoJSON vector overlay template.
+    """
+
+    # read template file and store in a string
+    with open(template_js_path, 'r') as f:
+        template_content = f.read()
+
+    geojsonbasename = os.path.basename(geojson_file_path)
+    # replace the placeholder with the GeoJSON file path
+
+    new_content = template_content.replace("url: 'GEOJSON.geojson'", f"url: '{geojsonbasename}'")
+
+    # write the new content to a new file
+    with open(destination_path, 'w') as f:
+        f.write(new_content)
+        
+    print(f'File created: {destination_path}')
 
 
-    # # Get the uploaded files from the widgets
-    # shapefiledata = iter(Shapefile_selector.value)
-    # for filename in shapefiledata:
-    #     with open(filename['name'], 'wb') as f:
-    #         f.write(filename['content'])
-    #     # set the shapefile name variable
-    #     ShpFileName = filename['name']
-    #     if ShpFileName.endswith('.shp'):
-    #         SHAPEFILE = ShpFileName
+def append_js_to_html(js_file_path, html_file_path, new_htmlfile_path):
+    """
+    Append a JS file to an HTML file before the </body> tag.
+
+    Args:
+        js_file_path (str): path to the JS file to append as source.
+        html_file_path (str): path to the HTML file.
+        new_htmlfile_path (str): path to the new HTML file.
+    """
+    # read the JS file
+    with open(js_file_path, 'r') as f:
+        js_content = f.read()
+
+    # read the HTML file
+    with open(html_file_path, 'r') as f:
+        html_content = f.read()
+
+    # Open the HTML file with BeautifulSoup
+    soup = BeautifulSoup(html_content, 'html.parser')
+    # create a new script source with beautifulsoup
+    srcbasename = os.path.basename(js_file_path)
+    script_tag = soup.new_tag('script', src=srcbasename)
+    if soup.body:
+        soup.body.append(script_tag)
+    else:
+        print('No <body> tag found in the HTML file.')
+
+    # write the new content to the HTML file
+    with open(new_htmlfile_path, 'w') as f:
+        f.write(str(soup))
+        
+    print(f'File updated: {new_htmlfile_path}')
---
+++ notebook/Test_new_features/test_PAL11/geojson.js
@@ -0,0 +1,23 @@
+// Add GeoJSON vector to the map
+var vectorSource = new ol.source.Vector({
+    url: 'PAL11.shp.geojson', // Path to GeoJSON
+    format: new ol.format.GeoJSON({
+        dataProjection: 'EPSG:3857',
+        featureProjection: 'EPSG:3857'
+    })
+});
+
+var vectorLayer = new ol.layer.Vector({
+    source: vectorSource,
+    style: new ol.style.Style({
+        stroke: new ol.style.Stroke({
+            color: 'blue',
+            width: 2
+        }),
+        fill: new ol.style.Fill({
+            color: 'rgba(0, 0, 255, 0.1)'
+        })
+    })
+});
+
+map.addLayer(vectorLayer);
