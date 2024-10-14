7e6547599cb0791eb003f39ea6e447582d8be261
Gianfranco Di Pietro <gianfrancodipietro@gmail,com>
2024-10-14 06:24:19 +0200

    testing new features

--- TESTING_zone/LIS/lis_functions.py
+++ TESTING_zone/LIS/lis_functions.py
@@ -47,7 +47,6 @@ def upload_files_widgets():
     
     return Shapefile_selector, Jpgfile_selector, projectname
 
-
 def save_to_temp_dir(Shapefile_selector, Jpgfile_selector, projectname):
     '''
     Save to temporary directory the file uploaded by the user
@@ -97,7 +96,6 @@ def save_to_temp_dir(Shapefile_selector, Jpgfile_selector, projectname):
     files_path['Shp_file_path'] = Shp_file_path
     return files_path
    
-
 def run_gdal2tiles(input_raster, output_directory):
     '''
     Run gdal2tiles.py command to create tiles from a raster file
@@ -129,6 +127,8 @@ def run_gdal2tiles(input_raster, output_directory):
 
 def create_geojson_js(geojson_file_path, template_js_path, destination_path):
     """
+    NOTE: deprecaded function
+
     Create a `destination_path` file with the GeoJSON file path.
     destination_path must be a .js file.
     destination path folder must be the same of the project name.
@@ -154,9 +154,10 @@ def create_geojson_js(geojson_file_path, template_js_path, destination_path):
     print(f'File created: {destination_path}')
     return
 
-
 def append_js_to_html(js_file_path, html_file_path, new_htmlfile_path):
     """
+    NOTE: deprecated function
+
     Append a JS file to an HTML file before the </body> tag.
 
     Args:
@@ -210,3 +211,181 @@ def convert_shp_to_geojson(input_shp, output_geojson):
         print(f'Conversion completed: {output_geojson}')
     except subprocess.CalledProcessError as e:
         print(f'{e}')
+
+    return
+
+def add_geojson_overlay_to_gdal2tiles_html_output(geojson_file_path, Html_path_input, Html_path_output):
+    """
+    Function to append a GeoJSON overlay to the HTML created with gdal2tiles.
+    NOTE: this function work with a specific version of OpenLayers library and
+    a specific version of gdal2tiles.
+    
+    Args:
+        geojson_file_path (str): path to the GeoJSON file.
+        Html_path_input (str): path to the HTML file to modify.
+        Html_path_output (str): path to the new HTML file.
+    """
+    # read html files:
+    with open(Html_path_input, 'r') as f:
+        html_content = f.read()
+    
+    # Open html with BeautifulSoup
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+
+    
+    # get the basename of the GeoJSON file path
+
+    geojsonURI = os.path.basename(geojson_file_path)
+
+    # Create new HTML contents (Javascript code, using double {{ to escape curly braces)}})
+    geojsonJSscript = f"""
+    // Add GeoJSON vector to the map
+    var vectorSource = new ol.source.Vector({{
+        url: '{geojsonURI}', // Path to GeoJSON
+        format: new ol.format.GeoJSON({{
+            dataProjection: 'EPSG:3857',
+            featureProjection: 'EPSG:3857'
+        }})
+    }});
+    var vectorLayer = new ol.layer.Vector({{
+        source: vectorSource,
+        style: new ol.style.Style({{
+            stroke: new ol.style.Stroke({{
+                color: 'blue',
+                width: 2
+            }}),
+            fill: new ol.style.Fill({{
+                color: 'rgba(0, 0, 255, 0.1)'
+            }})
+        }})
+    }});
+    map.addLayer(vectorLayer);
+    """
+    # add script to soup
+    script_tag = soup.new_tag('script')
+    script_tag.string = geojsonJSscript
+    if soup.body:
+        soup.body.append(script_tag)
+    else:
+        print('Error accessing to html file')
+
+    # Save the new HTML file
+    with open(Html_path_output, 'w') as f:
+        f.write(str(soup))
+    
+    print(f'File updated: {Html_path_output}')
+
+    return
+
+def add_popup_feature_to_gdal2tiles_html_output(Html_path_input, Html_path_output):
+
+    """
+    Function to append JS and CSS link of PopUp Feature to the HTML created with 
+    gdal2tiles and %append_js_to_html function.
+    NOTE: this function work with a specific version of OpenLayers-Extension library
+    Change the version of the library if needed.
+
+    Args:
+        Html_path (str): path to the html file to modify
+    """
+
+    # read html files:
+    with open(Html_path_input, 'r') as f:
+        html_content = f.read()
+    
+    
+
+    # Open html with BeautifulSoup
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    # Create new HTML contents
+
+    #OPENLAYERS-EXTENSION LIBRARY
+    Ol_ext_CSS = soup.new_tag('link', href="https://cdn.jsdelivr.net/npm/ol-ext@4.0.23/dist/ol-ext.min.css", rel="stylesheet")
+    Ol_ext_JS = soup.new_tag('script', src="https://cdn.jsdelivr.net/npm/ol-ext@4.0.23/dist/ol-ext.min.js", attrs={'id': 'olextJS'}) 
+    # add CSS link to the head
+    if soup.head:
+        soup.head.append(Ol_ext_CSS)
+    else:
+        print('Error accessing to html file')
+    # add JS link to the body
+    if soup.body:
+        soup.body.append(Ol_ext_JS)
+    else:
+        print('Error accessing to html file')
+        
+    # STEP 2 POPUP FEATURE
+    # Javasript code:
+    popup_script = """
+    // Select  interaction
+    var select = new ol.interaction.Select({
+        hitTolerance: 5,
+        multi: true,
+        condition: ol.events.condition.singleClick
+    });
+    map.addInteraction(select);
+
+    // popup
+    var popup = new ol.Overlay.PopupFeature({
+    popupClass: 'default anim',
+    select: select,
+    canFix: true,
+ 
+    template: {
+        title: 
+          // 'nom',   // only display the name
+          function(f) {
+            return f.get('Mineral');
+          },
+        attributes: //
+            {
+            'O' : { 
+                    title: 'Orientation', 
+                    before:'', 
+                    format: ol.Overlay.PopupFeature.localString(), // format as local string APPROX
+                    after:'°' 
+                },
+            'AsR' : { title: 'Aspect Ratio' },
+            'R': { title: 'Roundness' },
+            'A': { title: 'Area' },
+            'GSI': {title: 'Grain Shape Index'} // ,
+            }
+        }
+    });
+    map.addOverlay (popup);
+
+    // Event on attribute click
+    popup.on('attribute', console.log);
+    """
+    # add script to soup
+    script_tag = soup.new_tag('script')
+    script_tag.string = popup_script
+    if soup.body:
+        soup.body.append(script_tag)
+    else:
+        print('Error accessing to html file')
+
+    # CSS code:
+    popup_CSS = """
+    .ol-popup h1 {font-size: 1.2em; background-size: 2em; padding-left: 3em; width: 100%;}
+    .ol-attribution ul {font-size: .8em;}
+    .ol-control button i, .ol-control.ol-bar, .ol-control button i {color: inherit;}
+    """
+    # add CSS to soup
+    style_tag = soup.new_tag('style')
+    style_tag.string = popup_CSS
+    if soup.head:
+        soup.head.append(style_tag)
+    else:
+        print('Error accessing to html file')
+
+
+    # Save the new HTML file
+    with open(Html_path_output, 'w') as f:
+        f.write(str(soup))
+    
+    print(f'File updated: {Html_path_output}')
+
+
+    return
